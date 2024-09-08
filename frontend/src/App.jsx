848d77dae5af9c1f845eecf1debc8a3de00721dc
Ijeoma Jahsway <uchennamebijay@gmail.com>
2024-09-08 03:29:24 +0100

    Switched to hash router for gh-pages static rendering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import QuestionBank from './pages/QuestionBank';
 import AboutPage from './pages/AboutPage';
